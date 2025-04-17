83eea6c5773fc470ca01fbec04f19dd2697792ae
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2025-04-17 14:29:42 -0700

    basically nothing

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('home.html')
+
+@app.route('/batchedit')
+def batch_staging():
+    return render_template('batch.thml')
